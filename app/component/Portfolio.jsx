1a9b887d9af924b157b4e871983fdf66020278a8
Ahmed Mahmoud <ahmedmahmoud0929@gmail.com>
2025-06-26 18:34:23 +0300

    Update Portfolio.jsx

--- app/component/Portfolio.jsx
+++ app/component/Portfolio.jsx
@@ -32,7 +32,7 @@ export default function Projects() {
   return (
     <>
       
-      <main className="min-h-screen bg-[#030b16] text-white pt-24 px-6 border-b-2 border-blue-500" id="portfolio">
+      <main className="min-h-screen bg-black text-white pt-24 px-6 border-b-2 border-blue-500" id="portfolio">
         <section className="max-w-6xl mx-auto mb-20">
           <h2 className="text-4xl font-bold text-center text-blue-400 mb-12">My Portfolio</h2>
           <div className="grid md:grid-cols-3 gap-8">
